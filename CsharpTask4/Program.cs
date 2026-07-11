9d9fec02779644acf16e4241ea49619ae4523a52
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-11 22:43:19 +0400

    Create comment of all tasks qustion

--- CsharpTask4/Program.cs
+++ CsharpTask4/Program.cs
@@ -1,9 +1,64 @@
-﻿namespace CsharpTask4
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CsharpTask4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //EASY(4 Tasks)
+            //Task 1 - Personalized Welcome Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 2 - Square Number Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 3 - Celsius to Fahrenheit Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 4 - Fixed Menu Display Function
+
+
+
+            ///////////////////////////////////////////////////
+            //MEDIUM(4 Tasks)
+            //Task 5 - Even or Odd Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 6 - Rectangle Area & Perimeter Functions
+
+
+            ///////////////////////////////////////////////////
+            //Task 7 - Grade Letter Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 8 - Countdown Function
+
+            //HARD(2 Tasks)
+            //Task 9 - Overloaded Multiply Function
+
+
+            ///////////////////////////////////////////////////
+            //Task 10 - Overloaded Area Calculator
+
+
+
+            ///////////////////////////////////////////////////
+            //ADVANCED(2 Tasks)
+            //Task 11 - Function - Based Calculator
+
+
+            ///////////////////////////////////////////////////
+            //Task 12 - Student Report Card Generator
 
 
         }
