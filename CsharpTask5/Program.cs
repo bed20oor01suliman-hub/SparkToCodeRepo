91c1d3193544402740aa37a5f8e69f41ef721973
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-14 23:10:32 +0400

    Add comments

--- CsharpTask5/Program.cs
+++ CsharpTask5/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1 - Fixed Grades Array
+            //Task 2 - Dynamic To-Do List
+            //Task 3 - Browsing History Stack
+            //Task 4 - Customer Service Queue
+
+            //Task 5 - Array Grade Range
+
+            //Task 6 - Filtered Shopping List
+
+            //Task 7 - High Score Podium
+
+            //Task 8 - Undo Last Action
+
+            //Task 9 - Grade Analyzer with Functions
+
+            //Task 10 - Print Queue Manager
+
+            
         }
     }
 }
