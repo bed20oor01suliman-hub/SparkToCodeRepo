52781738963169c53296f8b00e686ab41f1ff5b8
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-15 22:33:28 +0400

    Complete task 6

--- CsharpTask5/Program.cs
+++ CsharpTask5/Program.cs
@@ -75,22 +75,53 @@
 
             /////////////////////////////
             //Task 5 - Array Grade Range
-            int[] grades = new int[5];
-             for(int i = 0; i < grades.Length; i++)
-            {
-                Console.Write("Please Enter "+ (i+1) + " Grade : ");
-                grades[i] = int.Parse(Console.ReadLine());
-                
-            }
-            Array.Sort(grades);
-            //double avgGrade = grades.Peak() / 5;
-            Console.WriteLine("Lowest Grade = " + grades[0]);
-            Console.WriteLine("Highest Grade = " + grades[4]);
-            //Two ways to get the average
-            Console.WriteLine("Average of all 5 Grades = " + grades.Average());
-            Console.WriteLine("Average of all 5 Grades = " + (grades[0]+ grades[1]+ grades[2]+ grades[3]+ grades[4])/5);
 
+            //int[] grades = new int[5];
+            // for(int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.Write("Please Enter "+ (i+1) + " Grade : ");
+            //    grades[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //Array.Sort(grades);
+            ////double avgGrade = grades.Peak() / 5;
+            //Console.WriteLine("Lowest Grade = " + grades[0]);
+            //Console.WriteLine("Highest Grade = " + grades[4]);
+            ////Two ways to get the average
+            //Console.WriteLine("Average of all 5 Grades = " + grades.Average());
+            //Console.WriteLine("Average of all 5 Grades = " + (grades[0]+ grades[1]+ grades[2]+ grades[3]+ grades[4])/5);
+
+            /////////////////////////////
             //Task 6 - Filtered Shopping List
+            bool exit = false;
+            List<string> shoppingList = new List<string>();
+            while (exit == false)
+            {
+                Console.WriteLine("Write your shoping list if you finish write (done): ");
+                string item = Console.ReadLine();
+                if (item.ToLower() == "done")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    shoppingList.Add(item.ToLower());
+                }
+            }
+            Console.WriteLine("Shopping List : ");
+            foreach(string item in shoppingList)
+            { 
+                Console.WriteLine(item.ToLower());
+            }
+            Console.WriteLine("Wirte one item you don't need : ");
+            shoppingList.Remove(Console.ReadLine().ToLower());
+
+            Console.WriteLine("Shopping List : ");
+
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine(item.ToLower());
+            }
 
             //Task 7 - High Score Podium
 
@@ -100,6 +131,13 @@
 
             //Task 10 - Print Queue Manager
 
+
+
+
+
+
+
+            ////////////////////////////////////////
             //Stack => LIFO
             //Push
             //Pop
@@ -110,7 +148,7 @@
             //Dequeue
             //Peek => value of first one
             //Count
-
+            ////////////////////////////////////////
         }
     }
 }
