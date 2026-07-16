ec7f380e7335e04e25bf8dcb99efc94141e490f8
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-16 20:27:22 +0400

    Create mini project

---
+++ C#MiniProject1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_MiniProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
