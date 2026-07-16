de45e763e1721ee165ef896593216d023b0a3a0d
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-17 00:50:37 +0400

    Start ansering the task

--- C#MiniProject1/Program.cs
+++ C#MiniProject1/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace C_MiniProject1
+﻿using System.Xml;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C_MiniProject1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //customerNames(List<string>) - the name of the customer who owns each account.
+            //accountNumbers(List<string>) - a unique account number for each account.
+            //balances(List<double>) - the current balance of each account.
+
+            List<string> customerNames = new List<string>();
+            List<string> accountNumbers = new List<string>();
+            List<string> balances = new List<string>();
         }
     }
 }
