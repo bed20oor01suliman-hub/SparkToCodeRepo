5440cf1ca685866db38a7042b8e9b99334c309a6
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-29 05:23:42 +0400

    working on database update to show in sql server but it still not done

---
+++ EFCoreProject/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreProject
+{
+    public class ProjectContext : DbContext //oop inhertance
+    {
+        //1. register  models
+               //DbSet == List
+        public DbSet<Employee> employees {  get; set; }
+        public DbSet<Department> department { get; set; }
+
+        //2. connect to database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                //.==LocalHost  
+                "Server=.;Database=CompanyProjectDB;Trusted_Connection=True;TrustServerCertificate=True;");//connection string
+        }
+
+    }
+}
