ab531c7806a271f40ba81a2ec0b7d34aa7e80f4b
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-07-29 22:15:07 +0400

    add data to employee table

--- EFCoreProject/Program.cs
+++ EFCoreProject/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace EFCoreProject
+﻿using EFCoreProject.Models;
+
+namespace EFCoreProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext context = new ProjectContext();
+            //add data on employees table
+            Employee e1 = new Employee();
+            e1.EmpName = "Badour";
+            e1.EmployeeSalary = 1000;
+            e1.EmployeeSSN = 262018;
+            e1.EmployeeAge = 24;
+            context.employees.Add(e1);//خطوة العمل
+            context.SaveChanges();//خطوة التأكيد
         }
     }
 }
