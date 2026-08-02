169c5a131c54dc368a574f3b664565d205c8f043
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-02 19:25:20 +0400

    make register of models and connection of Database

--- WebAPICoreProject/Program.cs
+++ WebAPICoreProject/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace WebAPICoreProject
 {
     public class Program
@@ -8,6 +10,8 @@ namespace WebAPICoreProject
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<ProjectContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
 
--- WebAPICoreProject/ProjectContext.cs
+++ WebAPICoreProject/ProjectContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAPICoreProject.Models;
 
 namespace WebAPICoreProject
 {
     public class ProjectContext : DbContext 
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
+        //database Connection
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+
+        } 
     }
 }
