2112580b615da7f6a59f8f43673081a889cc5b82
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-02 15:59:17 +0400

    write attributes for each model (Category & Product)

--- WebAPICoreProject/Models/Category.cs
+++ WebAPICoreProject/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace WebAPICoreProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPICoreProject.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
     }
 }
--- WebAPICoreProject/Models/Product.cs
+++ WebAPICoreProject/Models/Product.cs
@@ -1,6 +1,14 @@
-﻿namespace WebAPICoreProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPICoreProject.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+
     }
 }
