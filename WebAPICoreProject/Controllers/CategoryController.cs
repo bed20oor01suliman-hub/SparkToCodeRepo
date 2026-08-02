f89a3b803ed795b280bc5b7c9e9f3b9b9ed7b065
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-03 00:38:20 +0400

    work on product controller and categorycontroller functions

---
+++ WebAPICoreProject/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using WebAPICoreProject.Models;
+
+namespace WebAPICoreProject.Controllers
+{
+    public class CategoryController
+    {
+        private ProjectContext context;
+
+        public CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+        public void AddCategory(Category c)
+        {
+
+            context.Categories.Add(c);
+            context.SaveChanges();
+        }
+
+        public void RemoveCategory(int id)
+        {
+            Category c = context.Categories.FirstOrDefault(c => c.CategoryID == id);
+            if (c == null)
+            {
+
+            }
+
+            else
+            {
+                context.Categories.Remove(c);
+                context.SaveChanges();
+            }
+
+        }
+        public Category GetCategory(int id)
+        {
+            Category c = context.Categories.FirstOrDefault(c => c.CategoryID == id);
+            return c;
+        }
+        public List<Category> GetAllCategories()
+        {
+            List<Category> categories = context.Categories.ToList();
+            return categories;
+        }
+
+    }
+}
---
+++ WebAPICoreProject/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPICoreProject.Models;
+
+namespace WebAPICoreProject.Controllers
+{
+
+    public class ProductController
+    {
+        //was done in EFCore
+        //ProjectContext context = new ProjectContext();
+        private ProjectContext context;
+
+        public ProductController(ProjectContext _context)
+        {
+            context = _context;
+        }
+        public void AddProduct(Product p)
+        {
+
+            //was done in EFCore
+            //Product p = new Product();
+            //Console.WriteLine("enter product name");
+            //p.ProductName = Console.ReadLine();
+
+            //Console.WriteLine("enter product description");
+            //p.ProductDescription = Console.ReadLine();
+
+            //Console.WriteLine("enter product price");
+            //p.ProductPrice = double.Parse(Console.ReadLine());
+
+            context.Products.Add(p);
+            context.SaveChanges();
+        }
+
+        public void RemoveProduct(int id)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductID == id );
+            if (p == null)
+            {
+
+            }
+
+            else
+            {
+                context.Products.Remove(p);
+                context.SaveChanges();
+            }
+
+        }
+        public void UpdateProductPrice(int id,double newPrice)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductID == id);
+            p.ProductPrice = newPrice;
+            context.SaveChanges();
+        }
+
+        public void UpdateProductName(int id, string newName)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductID == id);
+            p.ProductName = newName;
+            context.SaveChanges(); 
+        }
+
+
+        //singl product
+        public Product GetProduct(int id)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductID == id);
+            return p;
+        }
+        //all products
+        public List<Product> GetAllProducts()
+        {
+            List<Product> products = context.Products.ToList();
+            return products;
+        }
+        public List<Product> GetByName(string name)
+        {
+            List<Product> products = context.Products.Where(p => p.ProductName.Contains(name)).ToList();
+            return products;
+        }
+
+    }
+}
